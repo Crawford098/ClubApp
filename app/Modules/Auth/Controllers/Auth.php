f05920712419828ea28c5b806057ae26ab7115dd
jonathan santana <santanajonathan098@gmail.com>
2023-09-27 14:32:17 -0400

    trabajando con la data

--- app/Modules/Auth/Controllers/Auth.php
+++ app/Modules/Auth/Controllers/Auth.php
@@ -6,7 +6,6 @@ use Inertia\Inertia;
 class Auth
 {
     private $fieldOfficeModel;
-
     public function __construct()
     {
         //Models
@@ -15,34 +14,35 @@ class Auth
 
     public function register()
     {
+
         $data = $this->fieldOfficeModel->getColumnBy([
-            'columns'       => ['field_officeId','code','typeId','parentId','name', 'level'],
-            'whereIn'       => ['typeId' => [4,5,6]]
+            'columns' => ['field_officeId','code','typeId','parentId','name', 'level'],
+            'whereIn' => ['typeId' => [4,5,6]]
         ]);
 
-//        dd(($this->objectToArray($data)));
-        dd($this->getChurchDropdown($this->objectToArray($data), 4));
+        $result = $this->getChurchDropdown( $this->objectToArray($data), 4);
+
+        dd($result);
 
         return Inertia::render('Auth/Register', [
             'churchesData' => $this->fieldOfficeModel->getColumnBy(['columns' => '',  'whereIn' => ['typeId' => [4,5,6]]])
         ]);
     }
 
-
     //TODO: Pasarlo a un helper
-    public function getChurchDropdown(array $data, int $level = 0, $parent = false) : array
+    public function getChurchDropdown(array $data, int $level = 0, $parentId = 0) : array
     {
         $result = [];
 
         foreach ($data as $key => $value)
         {
-            $parentCondition = $parent && $parent == $value['parent'];
+            $conditional = ($parentId && $parentId == $value['parentId'] || $parentId == 0);
 
-            if ($value['level'] == $level && $parentCondition)
+            if ($level == $value['level'] && ($parentId == 0 || $parentId == $value['parentId']))
             {
-                $result[$value['value']]       = $value['field_officeId'];
-                $result[$value['label']]       = $value['name'];
-                $result[$value['children']]    = $this->getChurchDropdown($data, $level++, $value['field_officeId']);
+                $result[$value['field_officeId']]['value']       = $value['field_officeId'];
+                $result[$value['field_officeId']]['label']       = $value['name'];
+                $result[$value['field_officeId']]['children']    = $this->getChurchDropdown( $data, $level++, $value['field_officeId']);
             }
         }
 
