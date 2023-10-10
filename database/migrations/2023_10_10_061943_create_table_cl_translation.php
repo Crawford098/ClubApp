3535b7031c1f812ff48264586853d681d0e94a82
Jonathan Santana <santanajonathan098@gmail.com>
2023-10-09 23:31:20 -0700

    Se agregaron unas migraciones para unos cambios en la base de datos

---
+++ database/migrations/2023_10_10_061943_create_table_cl_translation.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cl_translation', function (Blueprint $table) {
+            $table->id('translationId');
+            $table->string('lang');
+            $table->string('value');
+            $table->string('key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cl_translation');
+    }
+};
