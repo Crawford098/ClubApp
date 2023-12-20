ba9dd90a55955de3e1939c925eaa3c3138fadefc
Jonathan Santana <santanajonathan098@gmail.com>
2023-12-19 19:16:28 -0700

    Se esta realizando el diseño de el modulo de miembros

---
+++ resources/js/Layouts/ModuleHeader.jsx
@@ -0,0 +1,5 @@
+export const ModuleHeader = ({title}) => {
+    return (
+        <></>
+    )
+}
--- resources/js/Pages/Modules/Members/Index.jsx
+++ resources/js/Pages/Modules/Members/Index.jsx
@@ -1,8 +1,9 @@
 import {Link} from '@inertiajs/react';
 import Template from '@/Layouts/Template';
 import SearchIcon from '@rsuite/icons/Search';
-import { Input, InputGroup } from 'rsuite';
+import { Input, InputGroup, SelectPicker} from 'rsuite';
 import PrimaryButton from "@/Components/PrimaryButton";
+import style from '../../../../css/Modules/members.module.css';
 import {FatButton} from "@/Components/UIComponents/Buttons/FlatButtom";
 
 const Index = () => {
@@ -12,6 +13,15 @@ const Index = () => {
         margin: "0px 24px"
     }
 
+    const data = [{
+        value: 1,
+        label: 'Grid'
+    },{
+        value: 2,
+        label: 'Table'
+    }
+    ];
+
     return(
         <Template>
             <div className="header flex w-full justify-between">
@@ -19,19 +29,28 @@ const Index = () => {
                     <h2>Members</h2>
                 </div>
                 <div>
-                    <InputGroup style={styles}>
-                        <Input placeholder="placeholder"/>
-                        <InputGroup.Addon>
-                            <SearchIcon />
-                        </InputGroup.Addon>
-                    </InputGroup>
-                    <FatButton/>
                 </div>
             </div>
 
-            <div className={"content-body"}>
+            <div className={"content-body " + style.filterContainer}>
+                <div>
+                    <h3 className={style.filterTitle}>Filters:</h3>
+                </div>
+                <div className="flex">
+                    <div className="flex w-full items-center">
+                        <InputGroup style={styles}>
+                            <Input placeholder="placeholder"/>
+                            <InputGroup.Addon>
+                                <SearchIcon />
+                            </InputGroup.Addon>
+                        </InputGroup>
 
-                <h1>Content...</h1>
+                        <SelectPicker data={data}/>
+                    </div>
+                    <div className="items-center">
+                        <PrimaryButton>New</PrimaryButton>
+                    </div>
+                </div>
             </div>
         </Template>
     );
