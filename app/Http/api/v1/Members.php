6d0bd30fbed52514eb777dee696b2748e54bb910
Jonathan Santana <santanajonathan098@gmail.com>
2024-01-16 23:05:46 -0700

    Se agrego la funcion de insertar miembros

--- app/Http/api/v1/Members.php
+++ app/Http/api/v1/Members.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Controller;
 use App\Models\Members\MembersModel;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class Members extends Controller
@@ -18,8 +19,43 @@ class Members extends Controller
         return $this->membersModel->all()->toArray();
     }
 
-    public function insert () : array
+    public function insert (Request $request) : array
     {
-        return [];
+        $validator = Validator::make($request->all(), [
+            'clubId'                        => 'required',
+            'classId'                       => 'required',
+            'club_types'                    => 'required',
+            'username'                      => 'required|alpha_num|min:5|unique:App\Models\Members\MembersModel,username',
+            'email'                         => 'required|unique:App\Models\Members\MembersModel,email|email',
+            'password'                      => 'required',
+            'first_name'                    => 'required',
+            'date_of_birth'                 => 'required|date|date_format:Y-m-d',
+            'baptism_date'                  => 'date|date_format:Y-m-d',
+            'directive_positionId'          => 'required',
+            'active'                        => 'required',
+        ],[
+            'email.required'                => 'El campo email es requerido',
+            'email.email'                   => 'Email no valido',
+        ]);
+
+        if ($validator->fails())
+        {
+            return ['result' => 0, 'error' => $validator->errors()];
+        }
+
+        $this->membersModel->create([
+            'clubId'                        => $request->clubId,
+            'classId'                       => $request->classId,
+            'club_types'                    => $request->club_types,
+            'username'                      => $request->username,
+            'email'                         => $request->email,
+            'password'                      => $request->password,
+            'first_name'                    => $request->first_name,
+            'date_of_birth'                 => $request->date_of_birth,
+            'directive_positionId'          => $request->directive_positionId,
+            'active'                        => $request->active,
+        ]);
+
+        return ['result' => 1];
     }
 }
