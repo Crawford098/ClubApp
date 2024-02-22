f7f170ba0e1bc97d6240a38c1fcc4e0e7df35e40
jonathan santana <santanajonathan098@gmail.com>
2024-02-22 17:23:20 -0400

    haciendo el formulario

--- resources/js/Pages/Modules/Members/MemberAdd.jsx
+++ resources/js/Pages/Modules/Members/MemberAdd.jsx
@@ -1,16 +1,50 @@
 import Template from "@/Layouts/Template";
-import { Card, Form, Input, Flex } from "antd";
+import { Card, Form, Input, Flex, Button } from "antd";
 
 const MemberAdd = () => {
+
+    const handleSubmit = (event) => {
+
+        console.log(1312);
+        console.log(event);
+
+        
+    }
+
     return (
         <Template>
             <Card>
                 <h3>MemberAdd</h3>
-                <Form>
+                <Form name="basic" onFinish={handleSubmit}>
                     <Flex style={content}>
-                        <Input placeholder="Name" />
-                        <Input placeholder="Lastname" />;
+                        <Form.Item
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your name!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="lastName"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your lastName!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
                     </Flex>
+                    <Button htmlType="submit">
+                        Submit
+                    </Button>
                 </Form>
             </Card>
         </Template>
